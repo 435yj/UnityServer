a5f2d61bab184cc8a351d35c72f19ffc833861b6
435yj <yujin050420@naver.com>
2023-05-07 15:18:41 +0900

    주석

---
+++ Scripts/MyPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPlayer : Player
+{
+	NetworkManager _network;
+
+	void Start()
+    {
+		StartCoroutine("CoSendPacket");
+		_network = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+	}
+
+	// 플레이어 위치 계속 보내줌
+	IEnumerator CoSendPacket()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(0.25f);
+
+			C_Move movePacket = new C_Move();
+			movePacket.posX = UnityEngine.Random.Range(-50, 50);
+			movePacket.posY = 0;
+			movePacket.posZ = UnityEngine.Random.Range(-50, 50);
+			_network.Send(movePacket.Write());
+		}
+	}
+}
